ce080c7f9468791af9736e5acbbc73da2c0d74b9
theossoft <theossoft@gmail.com>
2021-02-10 15:43:25 +0300

    HeadFirst. BeerAdviser. Spinner. Выбор из выпадающего списка. StringBuilder.

--- headfirst_beeradviser/src/main/java/com/example/headfirst_beeradviser/MainActivity.java
+++ headfirst_beeradviser/src/main/java/com/example/headfirst_beeradviser/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
         //Получить вариант, выбранный в Spinner
         String beerType = String.valueOf(color.getSelectedItem());
         //Вывести выбранный вариант
-        brands.setText(beerType);
+        //brands.setText(beerType);
+        //Получить рекомендации от класса BeerExpert
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+//Вывести сорта пива
+        brands.setText(brandsFormatted);
+
     }
 }
