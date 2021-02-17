7c48e8d24441ba23e02733403439cab393b6ea7c
theossoft <theossoft@gmail.com>
2021-02-17 16:19:44 +0300

    HeadFirst. MyMessenger. Intent явный и неявный. Chooser.

---
+++ headfirst_mymessenger/src/main/java/com/example/headfirst_mymessenger/CreateMessageActivity.java
@@ -0,0 +1,32 @@
+package com.example.headfirst_mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Вызвать onSendMessage() при щелчке на кнопке
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        //startActivity(intent);
+        startActivity(chosenIntent);
+    }
+}
