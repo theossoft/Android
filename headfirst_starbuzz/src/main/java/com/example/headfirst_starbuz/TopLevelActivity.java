99c3851be74b3e447d3f1d1ccaed8bcdb41b1928
theossoft <theossoft@gmail.com>
2021-03-26 13:05:06 +0300

    HeadFirst. StarBuzz

---
+++ headfirst_starbuzz/src/main/java/com/example/headfirst_starbuz/TopLevelActivity.java
@@ -0,0 +1,36 @@
+package com.example.headfirst_starbuz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+//Создать OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> listView,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        //Добавить слушателя к списковому представлению
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
+
+
